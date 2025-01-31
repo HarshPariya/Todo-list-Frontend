635acec3c6c0dc761492a77fce72c86ae0f6af8c
Harsh-Pariya <harshpariya195@gmail.com>
2025-01-31 10:12:09 +0530

    first commit

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,29 +1,41 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function RegisterForm() {
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState();
+  const [password, setPassword] = useState('');
 
-function onSubmit(e){
-  e.preventDefault()
-}
+  function onSubmit(e) {
+    e.preventDefault();
+    const data = {
+      email,
+      password,
+    };
+
+    console.log('Form Submitted', data);
+  }
+
   return (
     <div>
-    <form method='POST' onSubmit={onSubmit} >
-      <div>
-      <h1>Register Form</h1>
-    <input type="text"
-     placeholder="Enter your Email" 
-     onChange={(e) => setEmail(e.target.value)} />
-     
-    <input type="password"
-     placeholder="Enter your Password"
-    onChange={(e) => setPassword(e.target.value)} />
-    <button type='submit'>Submit</button>
-      </div>
-    </form>
+      <form method='POST' onSubmit={onSubmit}>
+        <div>
+          <h1>Register Form</h1>
+          <input 
+            type="text"
+            placeholder="Enter your Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input 
+            type="password"
+            placeholder="Enter your Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type='submit'>Submit</button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
-export default RegisterForm
+
+export default RegisterForm;
